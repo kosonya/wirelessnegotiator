5b960c6ff5fca80c4edd10ae46ff3bf5c5785fc4
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2015-04-01 16:51:42 -0700

    somehow works

--- negotiator.py
+++ negotiator.py
@@ -27,6 +27,12 @@ class Entity(object):
 	
 	def tick(self):
 		pass
+	
+	def talk(self):
+		pass
+
+	def listen(self):
+		pass
 
 class WifiAP(Entity):
 	def __init__(self, BSSID, SSID, communication_bus, connection_scope, connection_speed, traffic_price, signal_strength, verbose = False):
@@ -39,15 +45,42 @@ class WifiAP(Entity):
 		self.signal_strength = signal_strength
 		self.verbose = verbose
 		self.available_bandwidth = connection_speed
+		self.partners = {} #{"AB:CD:EF:FE:DC:BA": {"negotiation_stage": "foo", "serving_bandwidth": 12345, "remaining_cycles": 10}}
 
 	def advertise(self):
 		message = {'message_type': 'offer', 'offer_type': 'connection', 'connection_scope': self.connection_scope, 'connection_speed': self.available_bandwidth, 'traffic_price': self.traffic_price, 'connection_type': 'wifi', 'signal_strength': self.signal_strength, 'message_source': self.BSSID, 'message_destination': 'all', 'BSSID': self.BSSID, 'SSID': self.SSID}
 		message_str = json.dumps(message)
 		self.combus.send(message_str)
 
+	def talk(self):
+		if self.available_bandwidth >= 0:
+			self.advertise()
+		for partner in self.partners.keys():
+			if self.partners[partner]["negotiation_stage"] == "contract_offered":
+				send_message =  {'message_type': 'contract_unilateral', 'contract_type': 'connection', 'connection_scope': self.connection_scope, 'connection_speed': self.partners[partner]["serving_bandwidth"], 'traffic_price': self.partners[partner]["price"], 'connection_type': 'wifi',  'message_source': self.BSSID, 'message_destination': partner, 'contract_length': self.partners[partner]["remaining_cycles"]}
+				send_message_str = json.dumps(send_message)
+				self.combus.send(send_message_str)
+			if self.partners[partner]["negotiation_stage"] == "serving":
+				if self.partners[partner]["remaining_cycles"] > 0:
+					send_message = {'message_type': 'service_token', 'service_type': 'connection', 'connection_scope': self.connection_scope, 'connection_speed': self.partners[partner]["serving_bandwidth"], 'traffic_price': self.partners[partner]["price"], 'connection_type': 'wifi',  'message_source': self.BSSID, 'message_destination': partner}
+					send_message_str = json.dumps(send_message)
+					self.combus.send(send_message_str)
+					self.partners[partner]["remaining_cycles"] -= 1
+				else:
+					del self.partners[partner]
+				
+
+
 	def tick(self):
-		self.advertise()
-		self.listen()
+		if self.verbose:
+			self.show()
+
+	def show(self):
+		print "\n", self.SSID, self.BSSID, "available bandwidth:", self.available_bandwidth
+		if self.partners:
+			for partner in self.partners.keys():
+				print self.partners[partner]
+		print "\n"
 
 	def listen(self):
 		messages = [json.loads(msg) for msg in self.combus.receive()]
@@ -55,22 +88,41 @@ class WifiAP(Entity):
 			if message['message_destination'] in [self.BSSID, 'all']:
 				if self.verbose:
 					print "Message received by %s: %s" % (self.BSSID, json.dumps(message))
+				if message['message_type'] == 'demand' and message['connection_type'] == 'wifi' and message['requested_bandwight'] <= self.available_bandwidth and message['requested_price'] >= self.traffic_price and message['message_source'] not in self.partners.keys():
+					self.partners[message['message_source']] = {"negotiation_stage": "contract_offered", "serving_bandwidth": message['requested_bandwight'], "remaining_cycles": message['requested_time'], "price":message['requested_price']}
+					self.available_bandwidth -= message['requested_bandwight']
+				if message['message_type'] == 'contract_signed' and message['message_source'] in self.partners.keys() and self.partners[message['message_source']]["negotiation_stage"] ==  "contract_offered":
+					self.partners[message['message_source']]["negotiation_stage"] = "serving"
+
 				
 
 class FreeWifiWanter(Entity):
-	def __init__(self, MAC, communication_bus, verbose = False):
+	def __init__(self, MAC, communication_bus, verbose = False, requested_service_time = 3):
 		self.MAC = MAC
 		self.combus = communication_bus
 		self.verbose = verbose
-		self.is_satisfied = False
-		self.is_negotiating = False
+		self.requested_service_time = requested_service_time
+		self.negotiation_stage = "looking for partners"
+		self.negotiation_party = None
 
-	def tick(self):
-		self.listen()
-		if self.is_negotiating:
-			message = {'message_type': 'demand', 'demand_type': 'connection', 'connection_type': 'wifi', 'message_source': self.MAC, 'message_destination': self.negotiating_party, 'requested_bandwight': 12345, 'requested_price': 0}
+	def talk(self):
+		if self.negotiation_stage == "sending demand":
+			message = {'message_type': 'demand', 'demand_type': 'connection', 'connection_type': 'wifi', 'message_source': self.MAC, 'message_destination': self.negotiation_party, 'requested_bandwight': 1234, 'requested_price': 0, 'requested_time': self.requested_service_time}
 			message_str = json.dumps(message)
 			self.combus.send(message_str)
+			self.negotiation_stage = "waiting for demand answer"
+		if self.negotiation_stage == "signing contract":
+			message = {'message_type': 'contract_signed', 'contract_type': 'connection', 'connection_type': 'wifi', 'message_source': self.MAC, 'message_destination': self.negotiation_party, 'requested_bandwight': 1234, 'requested_price': 0, 'requested_time': self.requested_service_time}
+			message_str = json.dumps(message)
+			self.combus.send(message_str)
+			self.negotiation_stage = "being served"
+
+	def tick(self):
+		if self.verbose:
+			self.show()
+
+	def show(self):
+		print "\n", self.MAC, "negotiation stage:", self.negotiation_stage, "negotiation party:", self.negotiation_party
 
 	def listen(self):
 		messages = [json.loads(msg) for msg in self.combus.receive()]
@@ -78,24 +130,30 @@ class FreeWifiWanter(Entity):
 			if message['message_destination'] in [self.MAC, 'all']:
 				if self.verbose:
 					print "Message received by %s: %s" % (self.MAC, json.dumps(message))
-				if not self.is_satisfied and not self.is_negotiating and message['message_type'] == 'offer' and message['offer_type'] == 'connection' and message['connection_type'] == 'wifi' and message['connection_scope'] == 'WAN' and message['traffic_price'] == 0:
+				if self.negotiation_stage == "looking for partners" and message['message_type'] == 'offer' and message['offer_type'] == 'connection' and message['connection_type'] == 'wifi' and message['connection_scope'] == 'WAN' and message['traffic_price'] == 0:
 						if self.verbose:
 							print "Found it:", message
-						self.is_negotiating = True
-						self.negotiating_party = message['message_source']
+						self.negotiation_stage = "sending demand"
+						self.negotiation_party = message['message_source']
+				if self.negotiation_stage == "waiting for demand answer" and message['message_type'] == 'contract_unilateral' and message['contract_type'] == 'connection' and message['connection_type'] == 'wifi' and message['connection_scope'] == 'WAN' and message['traffic_price'] == 0 and message['contract_length'] == self.requested_service_time:
+					self.negotiation_stage = "signing contract"
 				
 def main():
-	combus = CommunicationBus(name = "Common Communication Bus", verbose = True)
+	combus = CommunicationBus(name = "Common Communication Bus", verbose = False)
 
 	cmu_ap = WifiAP(BSSID = "12:34:56:78:90:AB", SSID = "CMU WiFi", communication_bus = combus, connection_scope = "WAN", connection_speed = 14400, traffic_price = 0, signal_strength = -40, verbose = True)
 
-	crossmobile_ap = WifiAP(BSSID = "21:43:65:87:09:BA", SSID = "Cross Mobile WiFi", communication_bus = combus, connection_scope = "LAN", connection_speed = 192000, traffic_price = 0, signal_strength = -60, verbose = True)
+	crossmobile_ap = WifiAP(BSSID = "21:43:65:87:09:BA", SSID = "Cross Mobile WiFi", communication_bus = combus, connection_scope = "LAN", connection_speed = 192000, traffic_price = 0, signal_strength = -60, verbose = False)
 
 	client = FreeWifiWanter(MAC = 'AB:CD:EF:FE:DC:BA', communication_bus = combus, verbose = True)
 
 	entities = [cmu_ap, crossmobile_ap, client]
 
 	while True:
+		for entity in entities:
+			entity.talk()
+		for entity in entities:
+			entity.listen()
 		for entity in entities:
 			entity.tick()
 		combus.tick()
